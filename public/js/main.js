f7088f291423dfe694a18e26e3c452f6e0079378
Al Leonard <aleonard@sparkgeo.com>
2024-10-07 16:31:12 -0400

    results are looking good

--- public/js/main.js
+++ public/js/main.js
@@ -1,107 +1,76 @@
-function toggleRows(className) {
-  const rows = document.querySelectorAll(`table tr.${className}`);
-
-  rows.forEach((row) => {
-    if (row.style.display === "none") {
-      row.style.display = "";
-    } else {
-      row.style.display = "none";
-    }
-  });
-  // location = "#table-wrapper-1";
-}
 function getOrdinalSuffix(place) {
+  //it's got to be a number
+  if (place === 0) {
+    return "-";
+  }
   if (place % 10 === 1 && place % 100 !== 11) return place + "st";
   if (place % 10 === 2 && place % 100 !== 12) return place + "nd";
   if (place % 10 === 3 && place % 100 !== 13) return place + "rd";
   return place + "th";
 }
+const races = {
+  1: {
+    title: "Steel City Alleycat",
+    date: "September 12, 2021",
+    about: "something something",
+  },
+  2: {
+    title: "Steel City Alleycat II",
+    date: "May 15, 2022",
+    about: "something something",
+  },
+  3: {
+    title: "Steel City Alleycat 3",
+    date: "October 16, 2022",
+    about: "something something",
+  },
+  4: {
+    title: "Steel City Alleycat 4",
+    date: "September 8, 2024",
+    about: "something something",
+  },
+};
+fetch("../results/all-results.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const racers = Object.values(data);
 
-function unixToHHMM(timestamp) {
-  const date = new Date(timestamp);
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const seconds = date.getSeconds().toString().padStart(2, "0");
-  return `${hours}:${minutes}:${seconds}`;
-}
-function getResults(file, raceno) {
-  fetch(file)
-    .then((response) => response.json())
-    .then((data) => {
-      const racers = Object.values(data);
-      const sortedData = racers
-        .filter((racer) => racer.finishTime > 0)
-        .sort((a, b) => a.finishTime - b.finishTime)
-        .concat(racers.filter((racer) => racer.finishTime === 0));
+    // Create the table and header row
 
-      // Create the table and header row
-      let table = `<table><tr>
-    <th>Place</th>
-    <th>Racer Name / #</th>
-    <th>Time</th>
-  </tr>`;
+    let table = "";
 
-      // Populate table rows
-      let place = 1;
-      sortedData.forEach((racer) => {
-        if (racer.finishTime > 0) {
-          table += `<tr ${
-            place > 3 ? "class='toggle1' style='display:none'" : ""
-          }>
-      <td>${getOrdinalSuffix(place++)}</td>
-      <td><span>${racer.racerNo}</span> ${racer.racerName}</td>
-      <td>${racer.finishTime ? unixToHHMM(racer.finishTime) : "DNF"}</td>
-    </tr>`;
+    let race = 1;
+    racers.forEach((racer) => {
+      if (race === racer.sca) {
+        race++;
+        console.log(`show header ${racer.sca}`);
+        if (race !== 1) {
+          table += "</table>";
         }
-      });
-
-      // Close the table tag
-      table += "</table>";
-
-      // Return the generated HTML table
-      // document.getElementById(`table-wrapper-${raceno}`).innerHTML = table;
-      // console.log(table);
+        table += `
+          <div class='results-header'>
+            <h3>${races[racer.sca].title}</h3>
+            <p>${races[racer.sca].date}</p>
+            <p class='aboutRace'>${races[racer.sca].about}</p>
+          </div>
+          <table>
+          <tr>
+            <th>Place</th>
+            <th>Racer Name / #</th>
+            <th>Time</th>
+          </tr>`;
+      }
+      table += `
+      <tr>
+        <td>${getOrdinalSuffix(racer.place++)}</td>
+        <td><span>${racer.number}</span> ${racer.name}</td>
+        <td>${racer.time === 0 ? "DNF" : racer.time}</td>
+      </tr>`;
     });
-}
 
-getResults("/results/results.json", 1);
-getResults("/results/results-ii.json", 2);
-getResults("/results/results-3.json", 3);
-// getResults("/results/results-4.json", 4);
+    // Close the table tag
+    table += "</table>";
 
-// function nav(href) {
-//   let template = href.split("/")[1];
-//   if (!template) {
-//     template = "home";
-//     location = "#/home";
-//   }
-//   //load hbs
-//   fetch(`../templates/${template}.hbs`)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error(`error ${response.status}`);
-//       }
-//       return response.text();
-//     })
-//     .then((fileContent) => {
-//       const template = Handlebars.compile(fileContent);
-//       const html = template({});
-//       document.getElementById("content").innerHTML = html;
-//     });
-// }
-// // all that's done, scroll up to the tippy top
-// window.scrollTo(0, 0);
-// // handle the navigation
-// $(document).ready(function () {
-//   nav(window.location.hash);
-//   // MAGIC!!
-//   window.onpopstate = function () {
-//     nav(window.location.hash);
-//   };
-//   history.pushState({}, "");
-//   // do the nav thing
-//   // $("nav a").click(function () {
-//   //   $("nav a").removeClass("active");
-//   //   $(this).addClass("active");
-//   // });
-// });
+    // Return the generated HTML table
+    document.getElementById(`table-wrapper`).innerHTML = table;
+  });
